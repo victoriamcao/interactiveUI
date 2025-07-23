bafd7b1eb9f35748b80f84acc311dc9abe64be75
Vicky Cao <miavickcao@gmail.com>
2025-07-23 14:27:07 -0700

    Initial Commit

---
+++ interactiveUI/interactiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactiveUIApp.swift
+//  interactiveUI
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct interactiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
